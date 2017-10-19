0ee1cba157c5de6ed29a283429c1ea18adc9b8a0
Joe Ryan <joe@clanryan.us>
2017-10-18 23:57:08 -0400

    fix set_board test to check for correct representation

--- tests/test_mancala.py
+++ tests/test_mancala.py
@@ -7,7 +7,7 @@ def test_mancala_init():
     assert my_game.board == [0]
 
 def test_mancala_configure_board():
-    board = [0, 1, 1, 3, 0, 0, 0]
+    board1 = [0, 1, 1, 3, 0, 0, 0]
     my_game = solitaire_mancala.SolitaireMancala()
-    my_game.set_board(board)
-    assert str(my_game) == '[0, 1, 1, 3, 0, 0, 0]'
+    my_game.set_board(board1)
+    assert str(my_game) == '[0, 0, 0, 3, 1, 1, 0]'
