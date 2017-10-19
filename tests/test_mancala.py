f50a81c2007765819c32401587c54caf2ff449bc
Joe Ryan <joe@clanryan.us>
2017-10-18 23:53:29 -0400

    add test for set_board

--- tests/test_mancala.py
+++ tests/test_mancala.py
@@ -5,3 +5,9 @@ import solitaire_mancala
 def test_mancala_init():
     my_game = solitaire_mancala.SolitaireMancala()
     assert my_game.board == [0]
+
+def test_mancala_configure_board():
+    board = [0, 1, 1, 3, 0, 0, 0]
+    my_game = solitaire_mancala.SolitaireMancala()
+    my_game.set_board(board)
+    assert str(my_game) == '[0, 1, 1, 3, 0, 0, 0]'
